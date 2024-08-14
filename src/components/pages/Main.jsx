0bb7bf34bfdf939210d718796da704cc0fb06679
madu5 <MM218@outlook.com.br>
2024-08-14 16:25:16 -0300

    created the functions create playlist and add tracks

--- src/components/pages/Main.jsx
+++ src/components/pages/Main.jsx
@@ -6,6 +6,8 @@ function Main() {
     const accessToken = localStorage.getItem('access_token');
     const [data, setData] = useState(null);
 
+    const [playlistId, setPlaylistId] = useState(''); // Temporary
+
     async function getProfile() {
         try {
             const response = await fetch('https://api.spotify.com/v1/me', {
@@ -16,10 +18,68 @@ function Main() {
 
             const result = await response.json();
             setData(result);
-            console.log('Profile', result);
             
         } catch (error) {
             console.error('Failed to get profile', error); 
+        };
+    };
+
+    async function createPlaylist() {
+        const playlistName = 'My Playlist From React'; // Temporary
+        const description = 'First playlist created with React by me to test for my app'; // Temporary
+
+        const url = `https://api.spotify.com/v1/users/${data.id}/playlists`;
+
+        const body = {
+            name: playlistName,
+            description,
+            public: true
+        };
+
+        try {
+            const response = await fetch(url, {
+                method: 'POST',
+                headers: {
+                    Authorization: `Bearer ${accessToken}`,
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(body)
+            });
+
+            const result = await response.json();
+            setPlaylistId(result.id); // Temporary
+            console.log('Playlist created', result); // Debug
+
+        } catch (error) {
+            console.error('Failed to create playlist', error);
+        }
+    };
+
+    async function addTracks() {
+        const tracks = [
+            'spotify:track:1QEt5w1GmR7opjz1kLUXbU',
+            'spotify:track:5M4Dxko4d3ja1CnIUmYVqA',
+            'spotify:track:78tx7xOBWGrPZacg3tYdX3',
+            'spotify:track:6dOtVTDdiauQNBQEDOtlAB'
+        ]; // Temporary
+
+        const url = `https://api.spotify.com/v1/playlists/${playlistId}/tracks`;
+
+        try {
+            const response = await fetch(url, {
+                method: 'POST',
+                headers: {
+                    Authorization: `Bearer ${accessToken}`,
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({ uris: tracks })
+            });
+
+            const result = await response.json();
+            console.log('Tracks added', result); // Debug
+
+        } catch (error) {
+            console.error('Failed to add tracks', error);
         };
     };
 
@@ -31,6 +91,13 @@ function Main() {
         <div>
             <h1>Main Page</h1>
             { data ? <User data={data}/> : <p>Não gerou token</p> }
+            
+            <button onClick={createPlaylist}>Create Playlist</button>
+            <button onClick={addTracks}>Add Tracks</button>
+
+            <a href={`https://open.spotify.com/playlist/${playlistId}`} target="_blank" rel="noopener noreferrer">
+                Go to Playlist
+            </a>
         </div>
     );
 };
