423a2dfb14f0732bfb7a677fb1907bec8bcc7b20
madu5 <MM218@outlook.com.br>
2024-08-13 23:48:12 -0300

    creating routes

--- src/App.js
+++ src/App.js
@@ -1,14 +1,26 @@
-import logo from './logo.svg';
+import React from 'react';
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
+
 import './App.css';
 
+import Landing from './components/pages/Landing';
+import Callback from './components/pages/Callback';
+import Main from './components/pages/Main';
+
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+
+            <BrowserRouter>
+                <Routes>
+                    <Route path='/' element={ <Landing /> } />
+                    <Route path='/callback' element={ <Callback /> } />
+                    <Route path='/app' element={ <Main /> } />
+                </Routes>
+            </BrowserRouter>
+        </div>
+    );
 }
 
 export default App;
