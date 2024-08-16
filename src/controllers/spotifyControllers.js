26dfaf80bf45b9306a6c1fb55366ec216f11eecb
madu5 <MM218@outlook.com.br>
2024-08-16 15:22:26 -0300

    refactoring spotify services

--- src/controllers/spotifyControllers.js
+++ src/controllers/spotifyControllers.js
@@ -2,63 +2,79 @@ import spotifyServices from '../services/spotifyServices';
 
 const spotifyControllers = {
     handleProfileRequest: async (accessToken, saveProfileInfo) => {
+        const url = 'https://api.spotify.com/v1/me';
+
         try {
-            const data = await spotifyServices.getProfile(accessToken);
+            const data = await spotifyServices.GETRequest(accessToken, url);
             if (data) saveProfileInfo(data);
         }
         catch (error) { console.error('Failed to request profile information', error) };
     },
 
     handlePlaylistCreation: async (userID, savePlaylistId, accessToken, playlistConfig) => {
+        const url = `https://api.spotify.com/v1/users/${userID}/playlists`;
+
         try {
-            const data = await spotifyServices.createPlaylist(userID, accessToken, playlistConfig);
+            const data = await spotifyServices.POSTRequest(accessToken, url, JSON.stringify(playlistConfig));
             if (data) savePlaylistId(data.id);
         }
         catch (error) { console.error('Failed to create playlist', error) };
     },
 
     handleTracksAddition: async (accessToken, playlistID, tracksIDs) => {
+        const url = `https://api.spotify.com/v1/playlists/${playlistID}/tracks`;
+
         try {
-            await spotifyServices.addTracks(accessToken, playlistID, tracksIDs);
+            await spotifyServices.POSTRequest(accessToken, url, JSON.stringify({ uris: tracksIDs }));
         }
         catch (error) { console.error('Failed to add tracks', error) };
     },
 
     handleTrackRequest: async (accessToken, trackID, saveTrack) => {
+        const url = `https://api.spotify.com/v1/tracks/${trackID}`;
+
         try {
-            const data = await spotifyServices.getTrack(accessToken, trackID);
+            const data = await spotifyServices.GETRequest(accessToken, url);
             if (data) saveTrack(data);
         }
         catch (error) { console.error('Failed to get track', error) };
     },
 
     handleGetFollowedArtists: async (accessToken) => {
+        const url = 'https://api.spotify.com/v1/me/following?type=artist';
+
         try {
-            const data = await spotifyServices.getFollowedArtists(accessToken);
+            const data = await spotifyServices.GETRequest(accessToken, url);
             return data.artists.items.slice(4, 7).map(artist => artist.id);
         }
         catch (error) { console.error('Failed to get followed artists', error) };
     },
 
     handleTopTracksRequest: async (accessToken, artistID, saveTopTracks) => {
+        const url = `https://api.spotify.com/v1/artists/${artistID}/top-tracks?market=BR`;
+
         try {
-            const data = await spotifyServices.getArtistTopTracks(accessToken, artistID);
+            const data = await spotifyServices.GETRequest(accessToken, url);
             if (data) saveTopTracks(data);
         }
         catch (error) { console.error('Failed to get top tracks', error) };
     },
 
     handleRelatedArtistsRequest: async (accessToken, artistID) => {
+        const url = `https://api.spotify.com/v1/artists/${artistID}/related-artists`;
+
         try {
-            const data = await spotifyServices.getRelatedArtists(accessToken, artistID);
+            const data = await spotifyServices.GETRequest(accessToken, url);
             return data.artists.slice(0, 5).map(artist => artist.id);
         }
         catch (error) { console.error('Failed to get related artists', error) };
     },
 
     handleRecommendationsRequest: async (accessToken, seedArtists, seedGenres, seedTracks) => {
+        const url = `https://api.spotify.com/v1/recommendations?seed_artists=${seedArtists}&seed_genres=${seedGenres}&seed_tracks=${seedTracks}`;
+
         try {
-            const data = await spotifyServices.getRecommendations(accessToken, seedArtists, seedGenres, seedTracks);
+            const data = await spotifyServices.GETRequest(accessToken, url);
             return data.tracks.filter(track => track.preview_url !== null);
         }
         catch (error) { console.error('Failed to get recommendations', error) };
--- src/services/spotifyServices.js
+++ src/services/spotifyServices.js
@@ -1,106 +1,23 @@
 const spotifyServices = {
-    getProfile: async (accessToken) => {
-        const response = await fetch('https://api.spotify.com/v1/me', {
+    GETRequest: async (accessToken, url) => {
+        const response = await fetch(url, {
             headers: {
                 Authorization: `Bearer ${accessToken}`
             }
         });
-    
+
         const data = await response.json();
         return data;
     },
 
-    createPlaylist: async (userID, accessToken, playlistConfig) => {
-        const url = `https://api.spotify.com/v1/users/${userID}/playlists`;
-
+    POSTRequest: async (accessToken, url, body) => {
         const response = await fetch(url, {
             method: 'POST',
             headers: {
                 Authorization: `Bearer ${accessToken}`,
                 'Content-Type': 'application/json'
             },
-            body: JSON.stringify(playlistConfig)
-        });
-
-        const data = await response.json();
-        return data;
-    },
-
-    addTracks: async (accessToken, playlistID, tracksIDs) => {
-        const url = `https://api.spotify.com/v1/playlists/${playlistID}/tracks`;
-
-        const response = await fetch(url, {
-            method: 'POST',
-            headers: {
-                Authorization: `Bearer ${accessToken}`,
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({ uris: tracksIDs })
-        });
-
-        const data = await response.json();
-        return data;
-    },
-
-    getTrack: async (accessToken, trackID) => {
-        const url = `https://api.spotify.com/v1/tracks/${trackID}`;
-
-        const response = await fetch(url, {
-            headers: {
-                Authorization: `Bearer ${accessToken}`
-            }
-        });
-
-        const data = await response.json();
-        return data;
-    },
-
-    getFollowedArtists: async (accessToken) => {
-        const url = 'https://api.spotify.com/v1/me/following?type=artist';
-
-        const response = await fetch(url, {
-            headers: {
-                Authorization: `Bearer ${accessToken}`
-            }
-        });
-
-        const data = await response.json();
-        return data;
-    },
-
-    getArtistTopTracks: async (accessToken, artistID) => {
-        const url = `https://api.spotify.com/v1/artists/${artistID}/top-tracks?market=BR`;
-        
-        const response = await fetch(url, {
-            headers: {
-                Authorization: `Bearer ${accessToken}`
-            }
-        });
-
-        const data = await response.json();
-        return data;
-    },
-
-    getRelatedArtists: async (accessToken, artistID) => {
-        const url = `https://api.spotify.com/v1/artists/${artistID}/related-artists`;
-
-        const response = await fetch(url, {
-            headers: {
-                Authorization: `Bearer ${accessToken}`
-            }
-        });
-
-        const data = await response.json();
-        return data;
-    },
-
-    getRecommendations: async (accessToken, seedArtists, seedGenres, seedTracks) => {
-        const url = `https://api.spotify.com/v1/recommendations?seed_artists=${seedArtists}&seed_genres=${seedGenres}&seed_tracks=${seedTracks}`;
-
-        const response = await fetch(url, {
-            headers: {
-                Authorization: `Bearer ${accessToken}`
-            }
+            body
         });
 
         const data = await response.json();
