e5588c3eb57eb63c2761d213b5e2b10be2ac20b3
madu5 <MM218@outlook.com.br>
2024-09-02 13:34:32 -0300

    refactoring code

--- src/pages/PlaylistMaker/MiddleSection.jsx
+++ src/pages/PlaylistMaker/MiddleSection.jsx
@@ -1,12 +1,50 @@
+import React from 'react';
 import { CSSTransition, TransitionGroup } from 'react-transition-group';
 
 import TrackPreview from './components/TrackPreview';
+
+import playlistMakerController from '../../controllers/playlistMakerController';
 
 import rejectLogo from '../../assets/reject.png';
 import addLogo from '../../assets/add.png';
 
 
-export default function MiddleSection({ suggestedTracks, rejectTrack, addTrackOnPlaylist, direction }) {
+export default function MiddleSection({ setPlaylist, accessToken }) {
+
+    const [suggestedTracks, setSuggestedTracks] = React.useState([]); // State to store the suggested tracks
+    const [direction, setDirection] = React.useState('left'); // State to control the direction of the card swipe
+
+    const addTrackOnPlaylist = () => {
+        const track = suggestedTracks[0];
+        setDirection('right');
+
+        setTimeout(() => {
+            setSuggestedTracks(prevSeggestions => prevSeggestions.slice(1));
+
+            setPlaylist(prevPlaylistContent => ({
+                ...prevPlaylistContent, tracks: [...prevPlaylistContent.tracks, track]
+            }));
+        }, 2);
+    };
+
+    const rejectTrack = () => {
+        setDirection('left');
+        setTimeout(() => { setSuggestedTracks(prevSeggestions => prevSeggestions.slice(1)) }, 2);
+    };
+
+    const handleKeyDown = (event) => {
+        if (event.key === 'ArrowRight') document.getElementById('add').click();
+        else if (event.key === 'ArrowLeft') document.getElementById('reject').click();
+    };
+
+    React.useEffect(() => {
+        //playlistMakerController.getTracksSuggestions(accessToken, setSuggestedTracks);
+
+        window.addEventListener('keydown', handleKeyDown);
+        return () => { window.removeEventListener('keydown', handleKeyDown) };
+    }, []);
+
+
     return (
         <div className='playlist-maker-center'>
             <h1>Playlist Maker Page</h1>
--- src/pages/PlaylistMaker/PlaylistMaker.jsx
+++ src/pages/PlaylistMaker/PlaylistMaker.jsx
@@ -14,41 +14,8 @@ function PlaylistMaker() {
     const userID = location.state;
     const accessToken = localStorage.getItem('access_token');
 
-    const [suggestedTracks, setSuggestedTracks] = useState([]); // State to store the suggested tracks
-    const [direction, setDirection] = useState('left'); // State to control the direction of the card swipe
     const [playlistContent, setPlaylistContent] = useState({ isDone: false, id: null, name: null, tracks: [] });
 
-
-    const addTrackOnPlaylist = () => {
-        const track = suggestedTracks[0];
-        setDirection('right');
-
-        setTimeout(() => {
-            setSuggestedTracks(prevSeggestions => prevSeggestions.slice(1));
-
-            setPlaylistContent(prevPlaylistContent => ({
-                ...prevPlaylistContent, tracks: [...prevPlaylistContent.tracks, track]
-            }));
-        }, 2);
-
-    };
-
-    const rejectTrack = () => {
-        setDirection('left');
-        setTimeout(() => { setSuggestedTracks(prevSeggestions => prevSeggestions.slice(1)) }, 2);
-    };
-
-    const handleKeyDown = (event) => {
-        if (event.key === 'ArrowRight') document.getElementById('add').click();
-        else if (event.key === 'ArrowLeft') document.getElementById('reject').click();
-    };
-
-    useEffect(() => {
-        //playlistMakerController.getTracksSuggestions(accessToken, setSuggestedTracks);
-
-        window.addEventListener('keydown', handleKeyDown);
-        return () => { window.removeEventListener('keydown', handleKeyDown) };
-    }, []);
 
     return (
         <div className='playlist-maker-container'>
@@ -57,10 +24,8 @@ function PlaylistMaker() {
             />
 
             <MiddleSection
-                suggestedTracks={suggestedTracks}
-                rejectTrack={rejectTrack}
-                addTrackOnPlaylist={addTrackOnPlaylist}
-                direction={direction}
+                setPlaylist={setPlaylistContent}
+                accessToken={accessToken}
             />
 
             <RightSection
